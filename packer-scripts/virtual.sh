019933f9fe8071e7c58b1666bb93e05559e5aff4
mainframed <mainframed767@gmail.com>
2020-10-28 22:57:12 -0700

    adding virtual classroom conf and setup script

---
+++ packer-scripts/virtual.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -eux
+echo "======> Adding VPN for Virtual Classes"
+chown root:root /tmp/evilmf.conf
+mv /tmp/evilmf.conf /etc/openvpn/client
+echo "======> Creating Dummy Open VPN creds"
+touch /etc/openvpn/client/up.txt
+echo "name" > /etc/openvpn/client/up.txt
+echo "password" >> /etc/openvpn/client/up.txt
+echo "======> Enabling openvpn with evilmf.conf"
+chmod 0600 /etc/openvpn/client/*
+systemctl enable openvpn-client@evilmf
+echo "======> Adding evilmf to /etc/hosts"
+echo "172.27.27.83 evilmf" >> /etc/hosts
+echo "172.27.27.83 evilmf.evilmainframes.com" >> /etc/hosts
+echo "======> Done with virtual edition"
