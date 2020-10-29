33643360442552d6529038071e66ba95987223bd
mainframed <mainframed767@gmail.com>
2020-10-28 23:06:34 -0700

    up not up.txt

--- packer-scripts/virtual.sh
+++ packer-scripts/virtual.sh
@@ -4,8 +4,8 @@ chown root:root /tmp/evilmf.conf
 mv /tmp/evilmf.conf /etc/openvpn/client
 echo "======> Creating Dummy Open VPN creds"
 touch /etc/openvpn/client/up.txt
-echo "name" > /etc/openvpn/client/up.txt
-echo "password" >> /etc/openvpn/client/up.txt
+echo "name" > /etc/openvpn/client/up
+echo "password" >> /etc/openvpn/client/up
 echo "======> Enabling openvpn with evilmf.conf"
 chmod 0600 /etc/openvpn/client/*
 systemctl enable openvpn-client@evilmf
